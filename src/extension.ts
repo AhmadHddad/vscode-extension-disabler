cd89f93c3eec3b70ca73391232d4086f49449faa
AhmadHddad <ahmadhddad@outlook.com>
2020-09-12 18:48:28 +0300

    the ability to delete profile and some code enhancemets

--- src/extension.ts
+++ src/extension.ts
@@ -1,189 +1,41 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as sqlite3 from "sqlite3";
-import * as utils from "./utils";
 import Config from "./config";
 import * as Commands from "./commands";
-
-const verbose = sqlite3.verbose();
-
-interface Extension {
-  id: number;
-  uuid: string;
-  label: string;
-}
-
-interface WorkSpaceOptions {
-  id: string;
-  configURIPath: string;
-}
-
-interface WorkSpaceForPick {
-  id: string;
-  label: string;
-}
-
-type SavedProfile = void | string;
-
-type SelectedWorkspace = WorkSpaceForPick | undefined;
-
-async function saveDisabledExtensionsToDB(
-  workspaceId: string,
-  extensionsToDisable: Array<Extension>
-) {
-  if (!extensionsToDisable.length) {
-    return;
-  }
-  let mappedExtensionsToDisable = JSON.stringify(
-    extensionsToDisable.map((ext) => ({
-      id: ext.id,
-      uuid: ext.uuid,
-    }))
-  );
-
-  const vsCodeRout = utils.getVsCodeRout();
-  const query = `INSERT INTO "main"."ItemTable" ("key", "value") VALUES ('extensionsIdentifiers/disabled','${mappedExtensionsToDisable}')`;
-  const dbRout = `${vsCodeRout}/User/workspaceStorage/${workspaceId}/state.vscdb`;
-
-  // open the database
-  let db = new verbose.Database(dbRout, sqlite3.OPEN_READWRITE, (err: any) => {
-    if (err) {
-      console.error(err.message);
-    }
-  });
-
-  await db.run(query, (err) => {
-    if (!err) {
-      vscode.window.showInformationMessage(
-        "Success!, Please restart workspace"
-      );
-      vscode.ConfigurationTarget.Global;
-    }
-  });
-
-  return;
-}
-
-function getWorkSpaceList(): Array<WorkSpaceOptions> {
-  const vsCodeRout = utils.getVsCodeRout();
-  let file = new Buffer("");
-
-  try {
-    file = fs.readFileSync(`${vsCodeRout}/storage.json`);
-  } catch (error) {
-    vscode.window.showErrorMessage("There are no workspaces registered!");
-    return [];
-  }
-  const workspaces3 = JSON.parse(file.toString()).openedPathsList.workspaces3;
-
-  return workspaces3.filter((opt: Object | String) => typeof opt === "object");
-}
-
-function getWorkSpaceForPick(): Array<WorkSpaceForPick> {
-  return getWorkSpaceList().map((option) => {
-    const splitPath = option.configURIPath.split("/");
-    return {
-      id: option.id,
-      label: splitPath[splitPath.length - 1],
-    };
-  });
-}
-
-function getAllExtensions(): Array<Extension> {
-  return vscode.extensions.all
-    .filter((ext) => !!ext.packageJSON.uuid)
-    .map((ext) => ({
-      id: ext.packageJSON.id,
-      uuid: ext.packageJSON.uuid,
-      label: ext.packageJSON.name,
-    }));
-}
-
-async function saveProfile(config: Config): Promise<SavedProfile> {
-  let profiles = config.getProfiles();
-
-  let profile = await vscode.window.showQuickPick(
-    [...profiles, "New profile"],
-    {
-      placeHolder: "Select a profile",
-    }
-  );
-
-  if (!profile || profile === "New profile") {
-    profile = await vscode.window.showInputBox({
-      placeHolder: "Enter the profile name",
-    });
-
-    if (!profile) {
-      return;
-    }
-
-    if (!profiles.length || profiles.indexOf(profile) === -1) {
-      await config.addProfile(profile);
-    }
-  }
-
-  return profile;
-}
-
-async function getSelectedWorkspace(): Promise<SelectedWorkspace> {
-  const workSpacesForList = getWorkSpaceForPick();
-
-  if (!workSpacesForList.length) {
-    vscode.window.showInformationMessage("Please add a workspace first");
-    return;
-  }
-
-  return await vscode.window.showQuickPick(workSpacesForList, {
-    placeHolder: "Select workspace",
-  });
-}
-
-async function selectExtensionsForDisable(): Promise<Array<Extension>> {
-  const allExtensions = getAllExtensions();
-
-  return (
-    (await vscode.window.showQuickPick(allExtensions, {
-      canPickMany: true,
-      placeHolder: "Select extensions to disable",
-    })) || []
-  );
-}
+import Services from "./services";
 
 export async function activate(context: vscode.ExtensionContext) {
-  let config = new Config(context);
+  const config = new Config(context);
+  const services = new Services();
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
       Commands.default.EXTENSION_DISABLER,
       async () => {
-        const selectedWorkspace = await getSelectedWorkspace();
+        const selectedWorkspace = await services.getSelectedWorkspace();
 
         if (!selectedWorkspace) {
           return;
         }
 
-        const savedProfile = await saveProfile(config);
+        const savedProfile = await services.saveProfile(config);
 
         if (!savedProfile) {
           return;
         }
 
-        const profileExtensions = await config.getProfileExtensions(
-          savedProfile
-        );
+        const profileExtensions = config.getProfileExtensions(savedProfile);
 
         if (
           !profileExtensions ||
           !profileExtensions?.selectedExtensionsForDisable.length
         ) {
-          const selectedExtensionsForDisable = await selectExtensionsForDisable();
+          const selectedExtensionsForDisable = await services.selectExtensionsForDisable();
           if (selectedExtensionsForDisable.length) {
             await config.addProfileExtensions(savedProfile, {
               selectedExtensionsForDisable: selectedExtensionsForDisable,
             });
             try {
-              await saveDisabledExtensionsToDB(
+              await services.saveDisabledExtensionsToDB(
                 selectedWorkspace.id,
                 selectedExtensionsForDisable
               );
@@ -194,13 +46,43 @@ export async function activate(context: vscode.ExtensionContext) {
             }
           }
         } else {
-          saveDisabledExtensionsToDB(
+          services.saveDisabledExtensionsToDB(
             selectedWorkspace.id,
             profileExtensions.selectedExtensionsForDisable
           );
         }
 
         return;
+      }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      Commands.default.EXTENSION_DISABLER_DELETE_PROFILE,
+      async () => {
+        let profiles = config.getProfiles();
+
+        if (profiles.length) {
+          const selectedProfile = await vscode.window.showQuickPick(profiles, {
+            placeHolder: "Select extensions to disable",
+          });
+
+          if (selectedProfile) {
+            await config.removeProfile(selectedProfile);
+            await config.removeProfileExtensions(selectedProfile);
+
+            await vscode.window.showInformationMessage(
+              `Profile ${selectedProfile} has been deleted.`
+            );
+          } else {
+            return;
+          }
+        } else {
+          vscode.window.showInformationMessage(
+            "You don't have any saved profiles!"
+          );
+        }
       }
     )
   );
