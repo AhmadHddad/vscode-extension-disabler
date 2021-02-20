8d55f98894ca061b4a236323ae3cb6fcda634be5
AhmadHddad <ahmadhddad@outlook.com>
2021-02-20 21:54:14 +0200

    adding  edit command

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,7 @@
 enum Commands {
   EXTENSION_DISABLER = "extension-disabler",
   EXTENSION_DISABLER_DELETE_PROFILE = "extension-disabler-delete-profile",
+  EXTENSION_DISABLER_EDIT_PROFILE = "extension-disabler-edit-profile",
 }
 
 export default Commands;
