7952559df461f93e011863e9f744e9a91b30eed7
AhmadHddad <ahmadhddad@outlook.com>
2021-02-20 21:54:48 +0200

    renaming to upper case and adding EDIT_PROFILE_OPTIONS

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,12 @@
-export const ConfigKey = "extensionDisabler";
+export const CONFIG_KEY = "extensionDisabler";
 
-export const ConfigProfilesKey = "profiles";
-export const ConfigStorageKey = "storage";
+export const CONFIG_PROFILES_KEY = "profiles";
+export const CONFIG_STORAGE_KEY = "storage";
+
+export const EDIT_OPTION_DELETE = 1;
+export const EDIT_OPTION_ADD = 0;
+
+export const EDIT_PROFILE_OPTIONS = [
+  { label: "Add More Extensions", value: EDIT_OPTION_ADD },
+  { label: "Delete Extensions", value: EDIT_OPTION_DELETE },
+];
